dd519f6ac772dc0dd6154f177e122c17e8fda9bc
NThompson480 <57776083+NThompson480@users.noreply.github.com>
2024-05-09 13:08:38 -0400

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -202,7 +202,7 @@ function Ensure-TerminalIconsInstalled {
 Ensure-TerminalIconsInstalled
 
 # Final check and message to the user
-if ((Test-Path -Path $PROFILE) -and (Test-ChocolateyPackageInstalled -packageName "oh-my-posh") -and ($fontFamilies -contains "CaskaydiaCove NF")) {
+if ((Test-Path -Path $PROFILE) -and (Test-ChocolateyPackageInstalled -packageName "oh-my-posh")) {
     Write-Host "Setup completed successfully. Please restart your PowerShell session to apply changes."
 } else {
     Write-Warning "Setup completed with errors. Please check the error messages above."
