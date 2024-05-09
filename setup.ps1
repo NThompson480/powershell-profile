9d64769a4123d5eefb2a58c937b810e9b306c15d
NThompson480 <57776083+NThompson480@users.noreply.github.com>
2024-05-09 13:22:49 -0400

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,13 +1,13 @@
 # Ensure the script can run with elevated privileges
 if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
     Write-Warning "Please run this script as an Administrator!"
-    break
+    exit
 }
 
 # Function to test internet connectivity
 function Test-InternetConnection {
     try {
-        $testConnection = Test-Connection -ComputerName www.google.com -Count 1 -ErrorAction Stop
+        Test-Connection -ComputerName www.google.com -Count 1 -ErrorAction Stop
         return $true
     }
     catch {
@@ -18,7 +18,7 @@ function Test-InternetConnection {
 
 # Check for internet connectivity before proceeding
 if (-not (Test-InternetConnection)) {
-    break
+    exit
 }
 
 # Function to get current date and time in a specific format
@@ -28,57 +28,40 @@ function Get-DateTimeStamp {
 
 # Profile creation or update
 if (!(Test-Path -Path $PROFILE -PathType Leaf)) {
+    $profilePath = $PSVersionTable.PSEdition -eq "Core" ? "$env:userprofile\Documents\Powershell" : "$env:userprofile\Documents\WindowsPowerShell"
+    if (!(Test-Path -Path $profilePath)) {
+        New-Item -Path $profilePath -ItemType Directory
+    }
+
     try {
-        # Detect version of PowerShell & create profile directories if they do not exist.
-        $profilePath = ""
-        if ($PSVersionTable.PSEdition -eq "Core") { 
-            $profilePath = "$env:userprofile\Documents\Powershell"
-        }
-        elseif ($PSVersionTable.PSEdition -eq "Desktop") {
-            $profilePath = "$env:userprofile\Documents\WindowsPowerShell"
-        }
-
-        if (!(Test-Path -Path $profilePath)) {
-            New-Item -Path $profilePath -ItemType "directory"
-        }
-
-        Invoke-RestMethod https://github.com/NThompson480/powershell-profile/raw/main/Microsoft.PowerShell_profile.ps1 -OutFile $PROFILE
-        Write-Host "The profile @ [$PROFILE] has been created."
-        Write-Host "If you want to add any persistent components, please do so at [$profilePath\Profile.ps1] as there is an updater in the installed profile which uses the hash to update the profile and will lead to loss of changes."
+        Invoke-RestMethod "https://github.com/NThompson480/powershell-profile/raw/main/Microsoft.PowerShell_profile.ps1" -OutFile $PROFILE
+        Write-Host "The profile @ [$PROFILE] has been created. Please add any persistent components to [$profilePath\Profile.ps1]."
     }
     catch {
-        Write-Error "Failed to create or update the profile. Error: $_"
+        Write-Error "Failed to create the profile. Error: $_"
     }
 }
 else {
     try {
-        # Rename existing profile appending the current date and time
         $dateTimeStamp = Get-DateTimeStamp
         $newProfileName = "$PROFILE.old_$dateTimeStamp"
         Rename-Item -Path $PROFILE -NewName $newProfileName
-
-        Invoke-RestMethod https://github.com/NThompson480/powershell-profile/raw/main/Microsoft.PowerShell_profile.ps1 -OutFile $PROFILE
-        Write-Host "The profile @ [$PROFILE] has been created and old profile renamed to [$newProfileName]."
-        Write-Host "Please back up any persistent components of your old profile to [$HOME\Documents\PowerShell\Profile.ps1] as there is an updater in the installed profile which uses the hash to update the profile and will lead to loss of changes."
+        Invoke-RestMethod "https://github.com/NThompson480/powershell-profile/raw/main/Microsoft.PowerShell_profile.ps1" -OutFile $PROFILE
+        Write-Host "The profile has been updated. Old profile is renamed to [$newProfileName]. Please backup any persistent components."
     }
     catch {
-        Write-Error "Failed to backup and update the profile. Error: $_"
+        Write-Error "Failed to update the profile. Error: $_"
     }
 }
 
-# Function to check if Chocolatey is installed
+# Function to check if Chocolatey is installed and install it if necessary
 function Ensure-ChocolateyInstalled {
-    $chocoPath = Get-Command choco -ErrorAction SilentlyContinue
-    if ($null -eq $chocoPath) {
+    if (-not (Get-Command "choco" -ErrorAction SilentlyContinue)) {
         Write-Host "Chocolatey not found. Attempting to install Chocolatey..."
         try {
-            # Set higher security protocol, necessary for accessing Chocolatey on HTTPS
             [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
-            
-            # Download and install Chocolatey
             Set-ExecutionPolicy Bypass -Scope Process -Force
             iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-            
             Write-Host "Chocolatey installed successfully."
         }
         catch {
@@ -90,24 +73,20 @@ function Ensure-ChocolateyInstalled {
     }
 }
 
-# Call the function to ensure Chocolatey is installed before proceeding with other installations
+# Ensure Chocolatey is installed before proceeding with other installations
 Ensure-ChocolateyInstalled
 
-# Improved function to install or update a Chocolatey package
+# Function to install or update a Chocolatey package
 function Install-ChocolateyPackage {
     param (
         [string]$packageName
     )
     try {
-        # Check if the package is already installed
         $localPackage = choco list --local-only $packageName -r
-
-        if ($localPackage) {
-            # Check if the package is up-to-date
-            $localVersion = $localPackage -split '\|' | Select-Object -Index 1
+        if ($localPackage -like "*$packageName*") {
+            $localVersion = ($localPackage -split '\|')[1]
             $remotePackage = choco search $packageName -r | Where-Object { $_ -like "$packageName|*" }
-            $remoteVersion = $remotePackage -split '\|' | Select-Object -Index 1
-
+            $remoteVersion = ($remotePackage -split '\|')[1]
             if ($localVersion -ne $remoteVersion) {
                 Write-Host "Updating $packageName from version $localVersion to $remoteVersion."
                 choco upgrade $packageName -y
@@ -124,13 +103,13 @@ function Install-ChocolateyPackage {
     }
 }
 
-# Oh My Posh Install or Update
+# Install or Update Oh My Posh
 Install-ChocolateyPackage -packageName "oh-my-posh"
 
-# zoxide Install or Update
+# Install or Update zoxide
 Install-ChocolateyPackage -packageName "zoxide"
 
-# Font Install
+# Font Installation
 try {
     [void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing")
     $fontFamilies = (New-Object System.Drawing.Text.InstalledFontCollection).Families.Name
@@ -166,18 +145,16 @@ function Test-ChocolateyPackageInstalled {
     param (
         [string]$packageName
     )
-    $installedPackages = choco list
+    $installedPackages = choco list --local-only
     return $installedPackages -like "*$packageName*"
 }
 
 # Function to install or update the Terminal-Icons module if necessary
 function Ensure-TerminalIconsInstalled {
     try {
-        # Check if Terminal-Icons is installed
         $installedModule = Get-Module -Name Terminal-Icons -ListAvailable
 
         if ($installedModule) {
-            # Check for updates
             $latestModule = Find-Module -Name Terminal-Icons
             if ($installedModule.Version -lt $latestModule.Version) {
                 Write-Host "Updating Terminal-Icons from version $($installedModule.Version) to $($latestModule.Version)."
@@ -187,7 +164,6 @@ function Ensure-TerminalIconsInstalled {
                 Write-Host "Terminal-Icons is up-to-date with version $($installedModule.Version)."
             }
         } else {
-            # Install the module if not installed
             Write-Host "Terminal-Icons is not installed. Installing now..."
             Install-Module -Name Terminal-Icons -Repository PSGallery -Force
             Write-Host "Terminal-Icons installed successfully."
@@ -198,12 +174,13 @@ function Ensure-TerminalIconsInstalled {
     }
 }
 
-# Call the function to ensure Terminal Icons is installed or updated
+# Ensure Terminal Icons is installed or updated
 Ensure-TerminalIconsInstalled
 
 # Final check and message to the user
-if ((Test-Path -Path $PROFILE) -and (Test-ChocolateyPackageInstalled -packageName "oh-my-posh") -and ($fontFamilies -contains "CaskaydiaCove NF")) {
+if ((Test-Path -Path $PROFILE) -and (Test-ChocolateyPackageInstalled -packageName "oh-my-posh") -and ($fontFamilies -contains "CaskaydiaCove NF") -and (Get-Module -Name Terminal-Icons -ListAvailable)) {
     Write-Host "Setup completed successfully. Please restart your PowerShell session to apply changes."
 } else {
     Write-Warning "Setup completed with errors. Please check the error messages above."
 }
+
